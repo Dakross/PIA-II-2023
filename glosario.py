292ed97c5530fc24d129cf5bcf4fd03e41e7b689
Dakross <91513194+Dakross@users.noreply.github.com>
2023-12-13 17:36:10 -0300

    depuracion codigo

---
+++ glosario.py
@@ -0,0 +1,55 @@
+# Define glosarios para cada sección
+glosario_capital_emocional = {
+    '1': 'Asertividad',
+    '2': 'Autoconciencia Emocional',
+    '3': 'Autoestima',
+    '4': 'Adaptabilidad',
+    '5': 'Autocontrol Emocional',
+    '6': 'Tolerancia a la Frustración',
+    '7': 'Motivación al Logro'
+}
+
+glosario_auto_reconocimiento = {
+    '1': 'Asertividad',
+    '2': 'Autoconciencia Emocional',
+    '3': 'Autoestima'
+}
+
+glosario_auto_regulacion = {
+    '4': 'Adaptabilidad',
+    '5': 'Autocontrol Emocional',
+    '6': 'Tolerancia a la Frustración',
+    '7': 'Motivación al Logro'
+}
+
+# Define glosarios para cada sección
+glosario_capital_social = {
+    '8': 'Comprensión Organizativa',
+    '9': 'Conciencia Crítica',
+    '10': 'Empatía',
+    '11': 'Percepción y Comprensión Emocional',
+    '12': 'Colaboración y Cooperación',
+    '13': 'Comunicación Asertiva',
+    '14': 'Desarrollar y Estimular a los demás',
+    '15': 'Desarrollo de las Relaciones',
+    '16': 'Influencia',
+    '17': 'Liderazgo',
+    '18': 'Manejo de Conflictos'
+}
+
+glosario_reconocimiento = {
+    '8': 'Comprensión Organizativa',
+    '9': 'Conciencia Crítica',
+    '10': 'Empatía',
+    '11': 'Percepción y Comprensión Emocional'
+}
+
+glosario_regulacion_social = {
+    '12': 'Colaboración y Cooperación',
+    '13': 'Comunicación Asertiva',
+    '14': 'Desarrollar y Estimular a los demás',
+    '15': 'Desarrollo de las Relaciones',
+    '16': 'Influencia',
+    '17': 'Liderazgo',
+    '18': 'Manejo de Conflictos',
+}
--- hola.py
+++ hola.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from flask import Flask, render_template, request, send_file
 from graph import grafico_frecuencia, grafico_puntaje
+from glosario import glosario_capital_emocional,glosario_capital_social,glosario_auto_regulacion,glosario_auto_reconocimiento,glosario_reconocimiento,glosario_regulacion_social
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -101,62 +102,6 @@ top_10_auto_regulacion = auto_regulacion.head(10)
 top_10_capital_social = capital_social.head(10)
 top_10_reconocimiento = reconocimiento.head(10)
 top_10_regulacion_social = regulacion_social.head(10)
-
-# Define glosarios para cada sección
-glosario_capital_emocional = {
-    '1': 'Asertividad',
-    '2': 'Autoconciencia Emocional',
-    '3': 'Autoestima',
-    '4': 'Adaptabilidad',
-    '5': 'Autocontrol Emocional',
-    '6': 'Tolerancia a la Frustración',
-    '7': 'Motivación al Logro'
-}
-
-glosario_auto_reconocimiento = {
-    '1': 'Asertividad',
-    '2': 'Autoconciencia Emocional',
-    '3': 'Autoestima'
-}
-
-glosario_auto_regulacion = {
-    '4': 'Adaptabilidad',
-    '5': 'Autocontrol Emocional',
-    '6': 'Tolerancia a la Frustración',
-    '7': 'Motivación al Logro'
-}
-
-# Define glosarios para cada sección
-glosario_capital_social = {
-    '8': 'Comprensión Organizativa',
-    '9': 'Conciencia Crítica',
-    '10': 'Empatía',
-    '11': 'Percepción y Comprensión Emocional',
-    '12': 'Colaboración y Cooperación',
-    '13': 'Comunicación Asertiva',
-    '14': 'Desarrollar y Estimular a los demás',
-    '15': 'Desarrollo de las Relaciones',
-    '16': 'Influencia',
-    '17': 'Liderazgo',
-    '18': 'Manejo de Conflictos'
-}
-
-glosario_reconocimiento = {
-    '8': 'Comprensión Organizativa',
-    '9': 'Conciencia Crítica',
-    '10': 'Empatía',
-    '11': 'Percepción y Comprensión Emocional'
-}
-
-glosario_regulacion_social = {
-    '12': 'Colaboración y Cooperación',
-    '13': 'Comunicación Asertiva',
-    '14': 'Desarrollar y Estimular a los demás',
-    '15': 'Desarrollo de las Relaciones',
-    '16': 'Influencia',
-    '17': 'Liderazgo',
-    '18': 'Manejo de Conflictos',
-}
 
 
 def index():
